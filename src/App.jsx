ed88386784caa19e7a98dcab8f3d56a011c0c840
EmmaOkafor <okaforifebuche26@gmail.com>
2024-08-16 13:12:11 +0100

    Added useCallBack hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -60,7 +60,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -71,15 +71,15 @@ function App() {
       'selectedPlaces', 
       JSON.stringify(storedIds.filter((id) !== selectedPlace.current ))
     );
-  }
+  },[]);
 
   return (
     <>
       <Modal open={modalIsOPen}>
-        <DeleteConfirmation
+        {modalIsOPen && <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
-        />
+        />}
       </Modal>
 
       <header>
